49f0ccdbe3695f3878c71214625dd219ba573b37
Carlos Lopes ✔ <lopes.carlos.host@gmail.com>
2023-09-06 22:19:30 -0300

    feat: modal for login page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,14 @@
+const modal = document.getElementById('modal');
+
 function nextPage() {
-  window.location.href = "../../assets/html/navers.html";
+  let nameValue = document.getElementsByName('name')[0].value;
+  let emailValue = document.getElementsByName('email')[0].value;
+  
+  localStorage.setItem('activeUser', JSON.stringify({name: nameValue, email: emailValue}));
+  modal.showModal();
 };
+
+document.getElementsByClassName('btn-close')[0].addEventListener('click', () => {
+  modal.close();
+  window.location.href = './assets/html/navers.html';
+});
