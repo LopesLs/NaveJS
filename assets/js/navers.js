43738609c9fd28ab3bf9a2d68d2fbb62b625325d
Carlos Lopes ✔ <lopes.carlos.host@gmail.com>
2023-09-06 22:19:00 -0300

    feat: btn logout for remove active user

--- assets/js/navers.js
+++ assets/js/navers.js
@@ -1,3 +1,8 @@
 document.getElementsByClassName('btn-add')[0].addEventListener('click', () => {
   window.location.href = "../html/addnavers.html"
 });
+
+document.getElementsByClassName('btn-logout')[0].addEventListener('click', () => {
+  localStorage.removeItem('activeUser');
+  window.location.href = "../../index.html"
+});
