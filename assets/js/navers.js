2d7b1381ed1460289d4a0e6725b7fc4e26ab885e
Carlos Lopes ✔ <lopes.carlos.host@gmail.com>
2023-09-08 22:35:29 -0300

    feat: new message when we don't have navers

--- assets/js/navers.js
+++ assets/js/navers.js
@@ -6,3 +6,12 @@ document.getElementsByClassName('btn-logout')[0].addEventListener('click', () =>
   localStorage.removeItem('activeUser');
   window.location.href = "../../index.html"
 });
+
+window.addEventListener('load', () => {
+  let navers = JSON.parse(localStorage.getItem('navers'));
+
+  if (!navers) {
+    let container = document.querySelector('.navers-container');
+    container.innerHTML = '<p>Nenhum naver encontrado</p>';
+  };
+});
