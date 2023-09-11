2fdf705ef3b1062b581724138290163301acb995
Carlos Lopes ✔ <lopes.carlos.host@gmail.com>
2023-09-10 22:34:40 -0300

    refactor: better code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,13 @@
 const modal = document.getElementById('modal');
 const closeButton = document.getElementsByClassName('btn-close')[0];
 
+window.addEventListener('load', () => {
+  let activeUser = JSON.parse(localStorage.getItem('activeUser'));
+  if (activeUser) {
+    window.location.href = './assets/html/navers.html';
+  }
+});
+
 function nextPage() {
   let nameValue = document.getElementsByName('name')[0].value;
   let emailValue = document.getElementsByName('email')[0].value;
@@ -14,11 +21,4 @@ closeButton.addEventListener('click', () => {
   modal.close();
   document.body.style.opacity = '1';
   window.location.href = './assets/html/navers.html';
-});
-
-window.addEventListener('load', () => {
-  let activeUser = JSON.parse(localStorage.getItem('activeUser'));
-  if (activeUser) {
-    window.location.href = './assets/html/navers.html';
-  }
 });
